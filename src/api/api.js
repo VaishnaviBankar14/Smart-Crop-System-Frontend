84c9ad44b100ab34b0b4568968f6063facb9664e
VaishnaviBankar14 <vbankar305@gmail.com>
2026-01-08 12:39:23 +0530

    Fix axios baseURL fallback

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://smart-crop-system-backend.onrender.com",
 });
 
 api.interceptors.request.use(
@@ -16,3 +18,23 @@ api.interceptors.request.use(
 );
 
 export default api;
+
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API_BASE_URL,
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
