1256fb6917b520150573a906aa120def8fb134a2
VaishnaviBankar14 <vbankar305@gmail.com>
2026-01-12 09:49:18 +0530

    Improve logout code

--- src/auth/Logout.jsx
+++ src/auth/Logout.jsx
@@ -1,8 +1,30 @@
-import "./Logout.css"
+// import "./Logout.css"
+// function Logout() {
+//   const logout = () => {
+//     localStorage.clear();
+//     window.location.reload();
+//   };
+
+//   return (
+//     <button
+//       className="btn btn-outline-light btn-sm logout-btn"
+//       onClick={logout}
+//     >
+//       Logout
+//     </button>
+//   );
+// }
+
+// export default Logout;
+import "./Logout.css";
+
 function Logout() {
   const logout = () => {
+    // 🔹 सगळं auth data clear
     localStorage.clear();
-    window.location.reload();
+
+    // 🔹 homepage ला redirect
+    window.location.href = "/";
   };
 
   return (
